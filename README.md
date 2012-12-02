42bcf294cb2ccf49d49ba55e07ce68be54b4b286
Neil Stoker <neil.stoker@gmail.com>
2012-12-02 17:08:14 +0000

    Added GPLv3 licence details to files

---
+++ README.md
@@ -0,0 +1,6 @@
+miniature-octo-robot
+====================
+
+A library for a simulation of a 6502 CPU.
+
+© 2012 Neil Stoker. Licensed under the GPL v3, see the file COPYING for further details
--- Sim65Main.cpp
+++ Sim65Main.cpp
@@ -4,8 +4,22 @@
  * Author:    Neil Stoker (neil.stoker@gmail.com)
  * Created:   2012-12-01
  * Copyright: Neil Stoker (https://sites.google.com/site/neilstoker/)
- * License:
- **************************************************************/
+ * License:   GPL v3
+              This file is part of Sim65.
+
+             Sim65 is free software: you can redistribute it and/or modify
+             it under the terms of the GNU General Public License as published by
+             the Free Software Foundation, either version 3 of the License, or
+             (at your option) any later version.
+
+             Sim65 is distributed in the hope that it will be useful,
+             but WITHOUT ANY WARRANTY; without even the implied warranty of
+             MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+             GNU General Public License for more details.
+
+             You should have received a copy of the GNU General Public License
+             along with Sim65.  If not, see <http://www.gnu.org/licenses/>.
+  **************************************************************/
 
 #ifdef WX_PRECOMP
 #include "wx_pch.h"
